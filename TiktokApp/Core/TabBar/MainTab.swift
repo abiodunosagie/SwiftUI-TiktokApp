cbdec63ffaa8c426f3e7da22d2eaa61a362b5fcf
Abiodun Osagie <abiodunosagie8@gmail>
2025-04-19 15:43:15 +0100

    tab view

--- TiktokApp/Core/TabBar/MainTab.swift
+++ TiktokApp/Core/TabBar/MainTab.swift
@@ -8,23 +8,37 @@
 import SwiftUI
 
 struct MainTab: View {
+    // MARK: - PROPERTIES
+    @State private var selectedTab = 0
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             Text("Feed")
                 .tabItem {
                     VStack {
-                        Image(systemName: "house")
+                        Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                            .environment(
+                                \.symbolVariants,
+                                 selectedTab == 0 ? .fill
+                                 : .none)
                         Text("Home")
                     }
                 }
+                .onAppear {selectedTab = 0}
+                .tag(0)
             
             Text("Explore")
                 .tabItem {
                     VStack {
-                        Image(systemName: "person.2")
+                        Image(systemName: selectedTab == 1 ? "person.2.fill" : "person.2")
+                            .environment(
+                                \.symbolVariants,
+                                 selectedTab == 1 ? .fill
+                                 : .none)
                         Text("Friends")
                     }
                 }
+                .onAppear {selectedTab = 1}
+                .tag(1)
             
             Text("Upload Post")
                 .tabItem {
@@ -33,23 +47,38 @@ struct MainTab: View {
                             .font(.system(size: 30,weight: .bold))
                     }
                 }
+                .onAppear {selectedTab = 2}
+                .tag(2)
             
             Text("Notifications")
                 .tabItem {
                     VStack {
-                        Image(systemName: "heart")
-                        Text("Home")
+                        Image(systemName:selectedTab == 3 ?  "heart.fill" : "heart")
+                            .environment(
+                                \.symbolVariants,
+                                 selectedTab == 3 ? .fill
+                                 : .none)
+                        Text("Notifications")
                     }
                 }
+                .onAppear {selectedTab = 3}
+                .tag(3)
             
             Text("Profile")
                 .tabItem {
                     VStack {
-                        Image(systemName: "person")
+                        Image(systemName:selectedTab == 4 ? "person.fill" : "person")
+                            .environment(
+                                \.symbolVariants,
+                                 selectedTab == 4 ? .fill
+                                 : .none)
                         Text("Profile")
                     }
                 }
+                .onAppear {selectedTab = 4}
+                .tag(4)
         }
+        .tint(.black)
     }
 }
 
